7acbc05e08444a7444a1edfb47406af91bc850c7
simolex <simolex@yandex.ru>
2022-01-31 20:05:09 +0300

    рефакторинг на ES6

--- les-01/script.js
+++ les-01/script.js
@@ -31,36 +31,28 @@ let data = {
   add: ["styled-components", "firebase"],
 };
 //stop
-const calcCash = (own) => {
+const calcCash = (own, ...everyCash) => {
   own = own || 0;
-  var everyCash = Array.prototype.slice.call(arguments);
-  var total = own;
-  for (var i = 0; i < everyCash[1].length; i++) {
-    total += +everyCash[1][i];
+  //var everyCash = Array.prototype.slice.call(arguments);
+  let total = own;
+  for (let i = 0; i < everyCash.length; i++) {
+    total += +everyCash[i];
   }
   return total;
 };
 
-var lesson = calcCash(null, data.cash);
+const lesson = calcCash(null, ...data.cash);
 
-function makeBusiness(director, teacher, allModule, gang, course) {
+const makeBusiness = (director, teacher, allModule, gang, course) => {
   teacher = teacher || "Максим";
-  var sumTech = data.react.concat(data.add, "и другие");
+  const sumTech = data.react.concat(data.add, "и другие");
   console.log(
-    'Стартуем новый курс: "' +
-      course +
-      '". Владелец: ' +
-      director +
-      ", преподаватель: " +
-      teacher +
-      ". Всего уроков: " +
-      allModule +
-      ". \nКоманда Академии: " +
-      gang
+    `Стартуем новый курс: "${course}". Владелец: ${director}, преподаватель: ${teacher}. Всего уроков: ${allModule}. 
+Команда Академии: ${gang}`
   );
-  console.log("Первое что изучим будет " + data.react[0] + ". Он очень похож на HTML!");
+  console.log(`Первое что изучим будет ${data.react[0]}. Он очень похож на HTML!`);
   console.log("Технологии которые мы изучим: ");
   console.log.apply(null, sumTech);
-}
+};
 
 makeBusiness.apply(null, ["Артем", null, lesson, command, nameCourse]);
