c4629196d9857fd956b761048d031e7fd0b3a4e3
simolex <simolex@yandex.ru>
2022-01-31 22:52:51 +0300

    Основы классового компонента

--- les-02-video1/indexClass.js
+++ les-02-video1/indexClass.js
@@ -5,6 +5,10 @@ const red = {
   fontSize: "24px",
 };
 
+const button = {
+  fontSize: "30px",
+};
+
 const alertHello = () => {
   alert("Hello");
 };
@@ -15,19 +19,38 @@ const Hello = (props) => (
   </p>
 );
 
-const RenderJSX = () => (
-  <div>
-    <header>
-      <img src="logo.svg" alt="logo react" className="logo" />
-      <h1>Hello React</h1>
-    </header>
-    <main>
-      <p>A JavaScript library for building UIs</p>
-      <Hello name="Alex" />
-      <Hello name="Nicoly" />
-      <Hello name="Fedor" />
-    </main>
-  </div>
-);
+class RenderJSX extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      counter: 0,
+    };
+    //this.changeCounter = this.changeCounter.bind(this); //передача this вариант 1
+  }
+  changeCounter = () => {
+    this.setState({
+      counter: this.state.counter + 1,
+    });
+  };
+  render() {
+    return (
+      <div>
+        <header>
+          <img src="logo.svg" alt="logo react" className="logo" />
+          <h1>Hello React</h1>
+        </header>
+        <main>
+          <button style={button} onClick={this.changeCounter}>
+            {this.state.counter}
+          </button>
+          <p>A JavaScript library for building UIs</p>
+          <Hello name="Alex" />
+          <Hello name="Nicoly" />
+          <Hello name="Fedor" />
+        </main>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<RenderJSX />, document.getElementById("root"));
