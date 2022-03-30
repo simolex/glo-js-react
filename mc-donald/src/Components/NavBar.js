167d12186320b2fbfe8ecd272e3568e779923e8f
simolex <simolex@yandex.ru>
2022-03-31 00:38:02 +0300

    Стилизация навбар

---
+++ mc-donald/src/Components/NavBar.js
@@ -0,0 +1,44 @@
+import React from "react";
+import styled from "styled-components";
+import logoImg from "../image/logo.svg";
+
+const NavBarStyled = styled.header`
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 999;
+  height: 80px;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 15px;
+  background-color: #299b01;
+  color: white;
+`;
+
+const Logo = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const H1 = styled.h1`
+  font-size: 24px;
+  margin-left: 15px;
+`;
+
+const ImgLogo = styled.img`
+  width: 50px;
+`;
+
+export const NavBar = () => {
+  return (
+    <NavBarStyled>
+      <Logo>
+        <ImgLogo src={logoImg} alt="logo" />
+        <H1>MrDonald's</H1>
+      </Logo>
+      <button>Войти</button>
+    </NavBarStyled>
+  );
+};
