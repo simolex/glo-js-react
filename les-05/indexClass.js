a64cc364b81abf05218a402b2000af967966683d
simolex <simolex@yandex.ru>
2022-03-31 14:00:18 +0300

    скопировал второй урок в пятый. Начинаем

---
+++ les-05/indexClass.js
@@ -0,0 +1,59 @@
+"use strict";
+
+const red = {
+  color: "#ff0000",
+  fontSize: "24px",
+};
+
+const button = {
+  fontSize: "30px",
+};
+
+const alertHello = () => {
+  alert("Hello");
+};
+
+const Hello = (props) => (
+  <p style={red} onClick={alertHello}>
+    Hello {props.name}
+  </p>
+);
+
+class RenderJSX extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      counter: 0,
+    };
+    //передача this вариант 1
+    //this.changeCounter = this.changeCounter.bind(this);
+  }
+  //передача this вариант 3 (стрелочная функция)
+  changeCounter = () => {
+    this.setState({
+      counter: this.state.counter + 1,
+    });
+  };
+  render() {
+    return (
+      <div>
+        <header>
+          <img src="logo.svg" alt="logo react" className="logo" />
+          <h1>Hello React</h1>
+        </header>
+        <main>
+          {/* передача this вариант 2  onClick={() => this.changeCounter()}*/}
+          <button style={button} onClick={this.changeCounter}>
+            {this.state.counter}
+          </button>
+          <p>A JavaScript library for building UIs</p>
+          <Hello name="Alex" />
+          <Hello name="Nicoly" />
+          <Hello name="Fedor" />
+        </main>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<RenderJSX />, document.getElementById("root"));
