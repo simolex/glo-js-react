6a8dfd6ad71e4ec066fb5b076d74059869efc97b
simolex <simolex@yandex.ru>
2022-04-12 10:14:17 +0300

    Оформлены заголовки в золотом стиле

--- mc-donald/src/Components/Menu/Menu.js
+++ mc-donald/src/Components/Menu/Menu.js
@@ -17,6 +17,24 @@ const SectionMenu = styled.section`
 `;
 
 const SectionTitle = styled.h2`
+  background: rgb(252, 242, 177);
+  background: linear-gradient(
+    175deg,
+    rgba(252, 242, 177, 1) 0%,
+    rgba(203, 191, 82, 1) 11%,
+    rgba(169, 140, 61, 1) 26%,
+    rgba(74, 61, 24, 1) 36%,
+    rgba(255, 230, 121, 1) 42%,
+    rgba(168, 151, 57, 1) 47%,
+    rgba(251, 241, 204, 1) 66%,
+    rgba(255, 255, 255, 1) 69%,
+    rgba(156, 136, 50, 1) 79%
+  );
+
+  filter: drop-shadow(1px 2px rgba(74, 61, 24, 0.5));
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-size: 400px;
   margin-bottom: 20px;
 `;
 
--- mc-donald/src/Components/Order/Order.js
+++ mc-donald/src/Components/Order/Order.js
@@ -21,9 +21,28 @@ const OrderStyled = styled.section`
 export const OrderTitle = styled.h2`
   text-align: center;
   text-transform: uppercase;
+
   font-size: 32px;
   line-height: 174%;
   margin-bottom: 15px;
+
+  background: rgb(252, 242, 177);
+  background: linear-gradient(
+    175deg,
+    rgba(252, 242, 177, 1) 0%,
+    rgba(203, 191, 82, 1) 11%,
+    rgba(169, 140, 61, 1) 26%,
+    rgba(74, 61, 24, 1) 36%,
+    rgba(255, 230, 121, 1) 42%,
+    rgba(168, 151, 57, 1) 47%,
+    rgba(251, 241, 204, 1) 66%,
+    rgba(255, 255, 255, 1) 69%,
+    rgba(156, 136, 50, 1) 79%
+  );
+
+  filter: drop-shadow(1px 2px rgba(74, 61, 24, 0.5));
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
 `;
 
 const OrderContent = styled.div`
