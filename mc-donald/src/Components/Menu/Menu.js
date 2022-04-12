014cb64c0b4695fd631f1fd9eb1cc88fd4d18692
simolex <simolex@yandex.ru>
2022-04-12 11:04:10 +0300

    цвет поменян в HEX-формат

--- mc-donald/src/Components/Menu/Menu.js
+++ mc-donald/src/Components/Menu/Menu.js
@@ -20,15 +20,15 @@ const SectionTitle = styled.h2`
   background: rgb(252, 242, 177);
   background: linear-gradient(
     175deg,
-    rgba(252, 242, 177, 1) 0%,
-    rgba(203, 191, 82, 1) 11%,
-    rgba(169, 140, 61, 1) 26%,
-    rgba(74, 61, 24, 1) 36%,
-    rgba(255, 230, 121, 1) 42%,
-    rgba(168, 151, 57, 1) 47%,
-    rgba(251, 241, 204, 1) 66%,
-    rgba(255, 255, 255, 1) 69%,
-    rgba(156, 136, 50, 1) 79%
+    #fcf2b0 0%,
+    #cbbf52 11%,
+    #a98c3d 26%,
+    #4a3d18 36%,
+    #ffe679 42%,
+    #a89739 47%,
+    #fbf1cc 66%,
+    #ffffff 69%,
+    #9c8832 79%
   );
 
   filter: drop-shadow(1px 2px rgba(74, 61, 24, 0.5));
